244f976492cca775c546450f5f080e71d0a1d027
Usuario <Usuario@CPD03-M04>
2024-03-11 15:59:41 -0300

    desenvolvimento do jogo da velha

--- MauiAppJogoDaVelha/MainPage.xaml.cs
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -23,12 +23,81 @@
             {
                 disparador.Text="X";
                 vez = "O";
-            }//Fecha if
+            }
+            //Fecha if
+
+
+            //verificando se o x ganhou 
+            if (btn10.Text == "X"
+               && btn11.Text == "X"
+               && btn12.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
+            if (btn20.Text == "X"
+                 && btn21.Text == "X"
+                 && btn22.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
+            if (btn30.Text == "X"
+                 && btn31.Text == "X"
+                 && btn32.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
+            if (btn10.Text == "X"
+                 && btn20.Text == "X"
+                 && btn30.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
+            if (btn11.Text == "X"
+                 && btn21.Text == "X"
+                 && btn31.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
+            if (btn12.Text == "X"
+                 && btn22.Text == "X"
+                 && btn32.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
+            if (btn12.Text == "X"
+                 && btn21.Text == "X"
+                 && btn30.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
+            if (btn10.Text == "X"
+                 && btn21.Text == "X"
+                 && btn32.Text == "X") ;
+            {
+                DisplayAlert("Parabéns!", "O X ganhou!", "OK");
+                limpar();
+            }
+
         }//Fecha Button_Clicked
 
-        private void btn11_Clicked(object sender, EventArgs e)
+        private void limpar()
         {
-
+            btn10.Text = "";
+            btn10.IsEnabled = true;
         }
     }
 
